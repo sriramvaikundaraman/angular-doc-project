ead198df09a4c78a2295897444d00b8544b9acb2
Sriram <sriramvaikundaraman@gmail.com>
2022-01-12 10:56:13 +0530

    changes

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,19 +8,19 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'iPhone 13 pro',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'iPhone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Nokia Standard',
     price: 299,
     description: ''
   },
