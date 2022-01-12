efab809fa0bb51d016497a8b31e400b0a65a437e
Sriram <sriramvaikundaraman@gmail.com>
2022-01-12 14:35:28 +0530

    updates

--- src/app/products.ts
+++ src/app/products.ts
@@ -10,25 +10,29 @@ export const products = [
     id: 1,
     name: 'iPhone 13 pro',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    image_path:'./assets/images/apple.jpg'
   },
   {
     id: 2,
     name: 'iPhone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    image_path:'./assets/images/mini.jpeg'
   },
   {
     id: 3,
-    name: 'Nokia Standard',
-    price: 299,
-    description: ''
+    name: 'Phone Note 5 Pro',
+    price: 499,
+    description: 'A great phone with one of the best processor',
+    image_path:'./assets/images/note5.jpeg'
   },
   {
     id: 4,
-    name: 'Phone Note 5 Pro',
-    price: 499,
-    description: 'A great phone with one of the best processor'
+    name: 'Nokia Standard',
+    price: 299,
+    description: 'Super battery Mobile',
+    image_path:'./assets/images/nokia.jpeg'
   }
 ];
 
